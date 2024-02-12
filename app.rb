f16ea25b991707a7cec2e28face0e41c0af4881a
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-02-12 15:17:32 +0000

    dice roll

--- app.rb
+++ app.rb
@@ -4,59 +4,19 @@ require "sinatra/reloader"
 get("/") do
   erb(:homepage)
 end
-et("/dice/2/6") do
-  first_die6 = rand(1..6)
-  second_die6 = rand(1..6)
-  sum6 = first_die6 + second_die6
-	
-  @outcome6 = "You rolled a #{first_die6} and a #{second_die6} for a total of #{sum6}."
-	
-
-
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  first_die20 = rand(1..20)
-  second_die20 = rand(1..20)
-  sum20 = first_die20 + second_die20
-	
-  @outcome20 = "You rolled a #{first_die20} and a #{second_die20} for a total of #{sum20}."
-	
-  erb(:one_twenty) 
-end
- 
-get("/dice/5/4") do
-  first_die4 = rand(1..5)
-  second_die4 = rand(1..5) 
-  third_die4 = rand(1..5)
-  four_die4 = rand(1..5)
-  five_die4 = rand(1..5) 
-  sum4 = first_die4 + second_die4 + third_die4 + four_die4 + five_die4
-	
-  @outcome4 = "You rolled a #{first_die4}, #{second_die4}, #{third_die4}, #{four_die4} and a #{five_die4} for a total of #{sum4}."
-	
-  erb(:five_four)
-end
 
 get("/dice/:alice/:bob") do
   @num_dice = params.fetch("alice").to_i
-  
+
   @sides = params.fetch("bob").to_i
 
   @rolls = []
 
-   @num_dice.times do
-      die = rand(1..@sides)
+  @num_dice.times do
+    die = rand(1..@sides)
 
-      @rolls.push(die) 
+    @rolls.push(die)
   end
 
   erb(:flexible)
 end
-
-
-
-   
- 
